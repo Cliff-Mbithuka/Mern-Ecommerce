1ea629858aadc06018372b83fd68db5f442c644b
Clifford Musyoka <your_email@example.com>
2024-06-18 13:55:32 +0300

    Connect Database

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config/config.env" });
+require('dotenv').config({ path: "./config/config.env" });
+const app = require("./app");
 const connectDataBase = require('./config/database')
 
-const app = require("./app");
-
-const PORT = process.env.PORT || 4000;
 connectDataBase();
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
          console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
