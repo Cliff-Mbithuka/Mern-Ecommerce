40590ea85d3d1b6e9d00af868e277cc6937dac10
Clifford Musyoka <your_email@example.com>
2024-06-25 05:35:11 +0300

    catch Async errors

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 const products = require('./routes/product');
+const errorMiddleware = require('./middlewares/errors')
+
 
 app.use(express.json());
 
 app.use('/api/v1/products', products);
+
+// Middleware to Handle Errors
+app.use(errorMiddleware);
 
 module.exports = app;
     
