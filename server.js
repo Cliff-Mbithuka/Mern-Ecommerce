54e70bc1a8174bc529d3b1d1403f507c75b89edc
Clifford Musyoka <your_email@example.com>
2024-06-25 05:47:45 +0300

    Handle unhandled promise errors

--- server.js
+++ server.js
@@ -5,8 +5,17 @@ const connectDataBase = require('./config/database')
 connectDataBase();
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
          console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
     });
+
+    //handle unhandled Promise rejection
+    process.on('unhandledRejection', err => {
+     console.log(`ERROR: ${err.message}`);
+     console.log(`Shutting down the server due to Unhandled Promise Rejections`);
+     server.close(() => {
+          process.exit(1)
+     })
+    })
 
     
