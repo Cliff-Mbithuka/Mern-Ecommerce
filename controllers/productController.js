3d0fe6ddac05e8b74bb8a2317b048b913528895c
Clifford Musyoka <your_email@example.com>
2024-06-25 08:02:25 +0300

    filter Products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
-
+const APIFeatures = require('../utils/apiFeatures') 
 
 // Create new product => /api/v1/product/new
 exports.newProduct = catchAsyncErrors (async (req, res, next) => {
@@ -11,7 +11,12 @@ exports.newProduct = catchAsyncErrors (async (req, res, next) => {
 
 // Get all product => /api/v1/products
 exports.getProducts = catchAsyncErrors (async (req, res, next) => {
-  const products = await Product.find();
+
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+.search()
+.filter()
+
+  const products = await apiFeatures.query;
 
   res.status(200).json({
     success: true,
